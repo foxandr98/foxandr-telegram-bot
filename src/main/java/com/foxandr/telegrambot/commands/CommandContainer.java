54723b659c05da9a9f4240e7b5bf061f7c9d5b93
Alex <foxandr98@mail.ru>
2023-08-20 00:55:52 +0500

    Добавлена библиотека org.reflections. Заполнение списка комманд реализовано через рефлексию

--- src/main/java/com/foxandr/telegrambot/commands/CommandContainer.java
+++ src/main/java/com/foxandr/telegrambot/commands/CommandContainer.java
@@ -3,9 +3,12 @@ package com.foxandr.telegrambot.commands;
 import com.foxandr.telegrambot.bot.TelegramBot;
 import com.foxandr.telegrambot.commands.base.StartCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 public class CommandContainer {
@@ -13,7 +16,17 @@ public class CommandContainer {
 
     public CommandContainer(TelegramBot telegramBot) {
         commandsByName = new HashMap<>();
-        addCommand(new StartCommand(telegramBot));
+        Reflections reflections = new Reflections("com.foxandr.telegrambot.commands");
+        Set<Class<? extends Command>> commandClasses = reflections.getSubTypesOf(Command.class);
+        for (Class<? extends Command> commandClass : commandClasses) {
+            log.info("COMMAND [{}] added to command container!", commandClass.getName());
+            try {
+                Command command = commandClass.getConstructor(TelegramBot.class).newInstance(telegramBot);
+                addCommand(command);
+            } catch (Exception e) {
+                log.error("Error while adding {} command to command container", commandClass.getName(), e);
+            }
+        }
     }
 
     private void addCommand(Command command) {
