fc7bf355504396a42dae4c977e57fc07eacd69bf
Alex <foxandr98@mail.ru>
2023-08-20 18:45:07 +0500

    Добавлены оболочки команд /start и /hello

---
+++ src/main/java/com/foxandr/telegrambot/commands/base/HelloCommand.java
@@ -0,0 +1,35 @@
+package com.foxandr.telegrambot.commands.base;
+
+import com.foxandr.telegrambot.bot.TelegramBot;
+import com.foxandr.telegrambot.commands.Command;
+import com.foxandr.telegrambot.util.MessageUtils;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class HelloCommand implements Command {
+    @Override
+    public String getName() {
+        return "/hello";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Сказать привет";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[]{"/hi", "/ку"};
+    }
+
+    @Override
+    public int getHelpOrderPriority() {
+        return 2;
+    }
+
+    @Override
+    public void execute(TelegramBot telegramBot, Message message, String[] args) throws TelegramApiException {
+        telegramBot.execute(MessageUtils.createSendMessageWithText(message,
+                "Привет, " + message.getFrom().getFirstName()));
+    }
+}
---
+++ src/main/java/com/foxandr/telegrambot/commands/base/StopCommand.java
@@ -0,0 +1,27 @@
+package com.foxandr.telegrambot.commands.base;
+
+import com.foxandr.telegrambot.bot.TelegramBot;
+import com.foxandr.telegrambot.commands.Command;
+import com.foxandr.telegrambot.util.MessageUtils;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class StopCommand implements Command {
+    @Override
+    public String getName() {
+        return "/stop";
+    }
+    @Override
+    public String getDescription() {
+        return "Отключить все подписки и таймеры";
+    }
+    @Override
+    public void execute(TelegramBot telegramBot, Message message, String[] args) throws TelegramApiException {
+        telegramBot.execute(MessageUtils.createSendMessageWithText(message, "Все подписки и таймеры отключены!"));
+    }
+
+    @Override
+    public int getHelpOrderPriority() {
+        return 1;
+    }
+}
